a1bf5e38700388cd5b8a661efde8cb7edb06b90f
sujoydey12 <sujoydey35@gmail.com>
2021-10-09 20:59:13 +0100

    Add changes

---
+++ src/main/java/com/example/kitchenengine/KitchenEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.kitchenengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenEngineApplication.class, args);
+	}
+
+}
